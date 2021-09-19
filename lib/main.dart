bccf1228e375879725e355c3efda4209034d3654
Kirill Ilichev <just.another.kirill.ilichev@gmail.com>
2021-09-20 02:57:43 +0300

    Added Firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_ecommerce_sample/bloc/cart_bloc/cart_bloc.dart';
@@ -5,14 +6,36 @@ import 'package:flutter_ecommerce_sample/bloc/products_bloc/products_bloc.dart';
 import 'package:flutter_ecommerce_sample/widget/navigation_wrapper.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final Future<FirebaseApp> _initializeFirebase = Firebase.initializeApp();
+
+  MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Future.wait([
+        _initializeFirebase,
+      ]),
+      builder: (_, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return _buildLoading();
+        }
+
+        if (snapshot.hasError) {
+          return _buildLoading(); // TODO
+        }
+
+        return _buildApp();
+      },
+    );
+  }
+
+  Widget _buildApp() {
     return MultiBlocProvider(
       providers: [
         BlocProvider<CartBloc>(create: (_) => CartBloc.initial()),
@@ -29,6 +52,15 @@ class MyApp extends StatelessWidget {
           ),
         ),
         home: const NavigationWrapper(index: 0),
+      ),
+    );
+  }
+
+  Widget _buildLoading() {
+    // TODO
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(child: CircularProgressIndicator()),
       ),
     );
   }
